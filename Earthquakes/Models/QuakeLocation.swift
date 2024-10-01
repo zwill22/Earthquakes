736fe3d3b021b45a95a534c1375960b79652ffe7
Zack Williams <73105122+zwill22@users.noreply.github.com>
2024-10-01 14:05:26 +0100

    Added struct to decode location from data

---
+++ Earthquakes/Models/QuakeLocation.swift
@@ -0,0 +1,51 @@
+//
+//  QuakeLocation.swift
+//  Earthquakes
+//
+//  Created by Zack Williams on 01/10/2024.
+//
+
+import Foundation
+
+struct QuakeLocation: Decodable {
+    var latitude: Double { properties.products.origin.first!.properties.latitude }
+    var longitude: Double { properties.products.origin.first!.properties.longitude }
+    private var properties: RootProperties
+    
+    struct RootProperties: Decodable {
+        var products: Products
+    }
+    
+    struct Products: Decodable {
+        var origin: [Origin]
+    }
+    
+    struct Origin: Decodable {
+        var properties: OriginProperties
+    }
+    
+    struct OriginProperties {
+        var latitude: Double
+        var longitude: Double
+    }
+}
+
+extension QuakeLocation.OriginProperties: Decodable {
+    private enum OriginPropertiesCodingKeys: String, CodingKey {
+        case latitude
+        case longitude
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: OriginPropertiesCodingKeys.self)
+        let latitude = try container.decode(String.self, forKey: .latitude)
+        let longitude = try container.decode(String.self, forKey: .longitude)
+        
+        guard let longitude = Double(longitude), let latitude = Double(latitude) else {
+            throw QuakeError.missingData
+        }
+        
+        self.latitude = latitude
+        self.longitude = longitude
+    }
+}
